8c64ef37efcb756d39fc42ef21a7b241cf4b9713
alinecintracamargo <alineandradecintra@gmail.com>
2024-07-02 10:47:50 -0300

    feat:  Associate Account to Supplier

--- db/migrate/20240618125931_create_parts.rb
+++ db/migrate/20240618125931_create_parts.rb
@@ -2,7 +2,7 @@ class CreateParts < ActiveRecord::Migration[7.1]
   def change
     create_table :parts do |t|
       t.string :part_number
-
+      t.belongs_to :supplier
       t.timestamps
     end
   end
